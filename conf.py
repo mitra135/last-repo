6c2e1d692ad68da2ac200d5ac1ccd49c0790ac1f
mitra135 <53474755+mitra135@users.noreply.github.com>
2021-02-18 16:50:20 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'EASE OF FARMING THROUGH IOT'
+copyright = '2021,MITRA'
+author = 'MITRAVINDA'
 
 # The full version, including alpha/beta/rc tags
 release = ''
